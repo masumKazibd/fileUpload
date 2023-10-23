2af5bae7de838273fd87a2f9e78d9ef4ded16b36
masumkazibd <masum.kazi.hantmk@gmail.com>
2023-10-23 15:50:21 +0600

    file upload

---
+++ CRUD_image/Controllers/FilemanagerController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_image.Controllers
+{
+    public class FilemanagerController : Controller
+    {
+        private IWebHostEnvironment _hostEnv;
+        public FilemanagerController(IWebHostEnvironment _hostEnv)
+        {
+            this._hostEnv = _hostEnv;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.msg = TempData["msg"];
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile fileToUpload)
+        {
+            string msg = "";
+            if (fileToUpload != null && fileToUpload.Length > 0)
+            {
+                string webroot= _hostEnv.WebRootPath;
+                string folder = "Images";
+                string fileName = Guid.NewGuid().ToString()+"_"+fileToUpload.FileName;
+                string fileToWrite = Path.Combine(webroot, folder, fileName);
+                //save
+                using (var stream = new FileStream(fileToWrite, FileMode.Create))
+                {
+                    await fileToUpload.CopyToAsync(stream);
+                    msg = "File [" + fileName + "] is uploaded successfully!!!";
+                    
+                }
+            }
+            else
+            {
+                msg = "Please select a valid file to upload!!";
+            }
+            TempData["msg"] = msg;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUD_image/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+//builder.Services.AddDbContext<GuestDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("appCon")));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
